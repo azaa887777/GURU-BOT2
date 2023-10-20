6f5a65af810a9ce6336436c6197791fc69250ced
azaa887777 <azaa88777@gmail.com>
2023-10-20 19:23:44 +0100

    Update fun-simsimi.js

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -1,37 +1,12 @@
 import fetch from 'node-fetch'
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+let handler = async (m, { text, usedPrefix, command }) => {
 if (!text) throw `اكتب نصا للتحدث معي\nمثال: ${usedPrefix + command} Hola bot*`
-await displayLoadingScreen(conn, m.chat, text)
+let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=ar`)
+let json = await res.json()
+let tes = json.success.replace('simsimi', 'simsimi').replace('Simsimi', 'Simsimi').replace('sim simi', 'sim simi')
+m.reply(`${tes}`) 
 }
 handler.help = ['simsimi']
 handler.tags = ['General']
 handler.command = ['bot', 'simi', 'simsimi'] 
 export default handler
-
-async function displayLoadingScreen(conn, from, text) {
-    const loadingStages = [];
-    let currentStage = "";
-  
-    for (let i = 0; i < text.length; i++) {
-        currentStage += text.charAt(i);
-        loadingStages.push(currentStage);
-    }
-  
-    try {
-        const { key } = await conn.sendMessage(from, { text: loadingStages[0] });
-  
-        for (let i = 1; i < loadingStages.length; i++) {
-            await conn.relayMessage(from, {
-                protocolMessage: {
-                    key: key,
-                    type: 14,
-                    editedMessage: {
-                        conversation: loadingStages[i]
-                    }
-                }
-            }, {});
-        }
-    } catch (error) {
-        console.error('Error displaying loading screen:', error);
-    }
-}
