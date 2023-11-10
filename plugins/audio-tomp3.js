9bfdaa0de8f214d2b93b2b786e32581f77cd2e2c
azaa887777 <azaa88777@gmail.com>
2023-11-10 22:47:33 +0100

    Update audio-tomp3.js

--- plugins/audio-tomp3.js
+++ plugins/audio-tomp3.js
@@ -1,17 +1,107 @@
-import { toAudio } from '../lib/converter.js'
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-    let q = m.quoted ? m.quoted : m
-   /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-    if (!/video|audio/.test(mime)) throw `✳️ Reply to the video or voice note you want to convert to mp3 with the command :\n\n*${usedPrefix + command}*`*/
-    let media = await q.download?.()
-    if (!media) throw '❎ Failed to download media'
-    let audio = await toAudio(media, 'mp4')
-    if (!audio.data) throw '❎ Error converting'
-    conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
+const handler = async (m, {conn, usedPrefix, command}) => {
+  const q = m.quoted ? m.quoted : m;
+  const mime = (q || q.msg).mimetype || q.mediaType || '';
+  if (!/video|audio/.test(mime)) throw `*ارسال الامر متبوع بالفيديو*`;
+  const media = await q.download();
+  if (!media) throw 'أنا آسف، حدث خطأ أثناء تنزيل الفيديو، يرجى المحاولة مرة أخرى*';
+  const audio = await toAudio(media, 'mp4');
+  if (!audio.data) throw '*أنا آسف، حدث خطأ أثناء تحويل الملاحظة الصوتية إلى صوت/MP3، يرجى المحاولة مرة أخرى*';
+  conn.sendMessage(m.chat, {audio: audio.data, mimetype: 'audio/mpeg'}, {quoted: m});
+};
+handler.alias = ['tomp3', 'toaudio'];
+handler.command = /^to(mp3|audio)$/i;
+export default handler;
+
+import {promises} from 'fs';
+import {join} from 'path';
+import {spawn} from 'child_process';
+
+function ffmpeg(buffer, args = [], ext = '', ext2 = '') {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const tmp = join(global.__dirname(import.meta.url), '../tmp', + new Date + '.' + ext);
+      const out = tmp + '.' + ext2;
+      await promises.writeFile(tmp, buffer);
+      spawn('ffmpeg', [
+        '-y',
+        '-i', tmp,
+        ...args,
+        out,
+      ])
+          .on('error', reject)
+          .on('close', async (code) => {
+            try {
+              await promises.unlink(tmp);
+              if (code !== 0) return reject(code);
+              resolve({
+                data: await promises.readFile(out),
+                filename: out,
+                delete() {
+                  return promises.unlink(out);
+                },
+              });
+            } catch (e) {
+              reject(e);
+            }
+          });
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
-handler.help = ['tomp3']
-handler.tags = ['fun']
-handler.command = /^to(mp3|a(udio)?)$/i
 
-export default handler
+/**
+ * Convert Audio to Playable WhatsApp Audio
+ * @param {Buffer} buffer Audio Buffer
+ * @param {String} ext File Extension
+ * @return {Promise<{data: Buffer, filename: String, delete: Function}>}
+ */
+function toPTT(buffer, ext) {
+  return ffmpeg(buffer, [
+    '-vn',
+    '-c:a', 'libopus',
+    '-b:a', '128k',
+    '-vbr', 'on',
+  ], ext, 'ogg');
+}
+
+/**
+ * Convert Audio to Playable WhatsApp PTT
+ * @param {Buffer} buffer Audio Buffer
+ * @param {String} ext File Extension
+ * @return {Promise<{data: Buffer, filename: String, delete: Function}>}
+ */
+function toAudio(buffer, ext) {
+  return ffmpeg(buffer, [
+    '-vn',
+    '-c:a', 'libopus',
+    '-b:a', '128k',
+    '-vbr', 'on',
+    '-compression_level', '10',
+  ], ext, 'opus');
+}
+
+/**
+ * Convert Audio to Playable WhatsApp Video
+ * @param {Buffer} buffer Video Buffer
+ * @param {String} ext File Extension
+ * @return {Promise<{data: Buffer, filename: String, delete: Function}>}
+ */
+function toVideo(buffer, ext) {
+  return ffmpeg(buffer, [
+    '-c:v', 'libx264',
+    '-c:a', 'aac',
+    '-ab', '128k',
+    '-ar', '44100',
+    '-crf', '32',
+    '-preset', 'slow',
+  ], ext, 'mp4');
+}
+
+export {
+  toAudio,
+  toPTT,
+  toVideo,
+  ffmpeg,
+};
