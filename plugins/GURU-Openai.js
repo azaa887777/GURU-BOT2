1c4a94d43fa9426c7ea9729faeafcf25ff8fa827
azaa887777 <azaa88777@gmail.com>
2023-11-24 18:46:45 +0100

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, match, args }) => {
    let q = m.quoted ? m.quoted : m;
-  if (!/text/.test(q.text)) return m.reply("*sift code*")
+  if (!args[0]) throw ' question ❓'
     await m.reply('جاري الاجابة...')
     let res = await fetch(`https://vihangayt.me/tools/chatgpt?q=${q.text.replace(usedPrefix + command, "")}`)
     let open = await res.json()
