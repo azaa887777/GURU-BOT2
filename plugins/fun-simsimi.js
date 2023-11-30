a19cf667c1268e8afc9d6844122b96016b505430
azaa887777 <azaa88777@gmail.com>
2023-11-30 19:43:08 +0100

    Update fun-simsimi.js

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
 if (!text) throw `اكتب نصا للتحدث معي\nمثال: ${usedPrefix + command} Hola bot*`
-let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=ar`)
+let res = await fetch(`https://simsimi.fun/api/v2/?mode=talk&lang=ar&message=${text}&filter=false`)
 let json = await res.json()
 let tes = json.success.replace('simsimi', 'simsimi').replace('Simsimi', 'Simsimi').replace('sim simi', 'sim simi')
 m.reply(`${tes}`) 
